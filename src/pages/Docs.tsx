9832d29d7205092ca668e11f821e8ea60efc4fde
chaitu426 <interiormern@gmail.com>
2025-06-28 19:04:55 +0530

    add

--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -1,16 +1,12 @@
-
 import { useEffect, useState } from "react";
 import Header from "@/components/Header";
 import DocsSidebar from "@/components/DocsSidebar";
 import DocsContent from "@/components/DocsContent";
 import setu from 'setu.js';
 
-
-
 const Docs = () => {
   const [activeSection, setActiveSection] = useState("introduction");
-
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<{ id: number; name: string; email: string }[]>([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -31,7 +27,14 @@ const Docs = () => {
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex flex-1">
-        <p>{users}</p>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-2">Fetched Users</h2>
+          <ul className="list-disc pl-5 space-y-1">
+            {users.map((user) => (
+              <li key={user.id}>{user.name} ({user.email})</li>
+            ))}
+          </ul>
+        </div>
         <DocsSidebar 
           activeSection={activeSection} 
           onSectionChange={setActiveSection}
